c9a75f094d01e514fbfe8824f9e84972be26a76a
Rüçhan Adıgüzel <jceadsa@gmail.com>
2022-03-12 12:38:17 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/school_project_ibdb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.school_project_ibdb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorConstants {
+  ///This color values will move to ThemeData
+  static final Color primaryColor = Color.fromARGB(0, 255, 255, 255);
+
+  ///This color values will move to ThemeData
+  static final Color secondaryColor = Color(0xff38D39F);
+}
